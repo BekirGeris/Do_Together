9ac48c1b09eb1287348478227c8b37d7ca7411e7
Bekir Geriş <bekir.geris@dgpaysit.com>
2022-10-25 01:08:20 +0300

    profile fragment düzenlemeleri

--- app/src/main/java/com/example/dotogether/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/dotogether/view/fragment/ProfileFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dotogether.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dotogether.databinding.FragmentProfileBinding
+import com.example.dotogether.model.Target
+import com.example.dotogether.view.adapter.TargetAdapter
+import java.util.ArrayList
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
 
+    private lateinit var targetAdapter: TargetAdapter
+    private val targets = ArrayList<Target>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        initViews()
     }
 
     override fun onCreateView(
@@ -24,4 +32,14 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    private fun initViews() {
+        for (i in 1..10) {
+            targets.add(Target())
+        }
+
+        targetAdapter = TargetAdapter(targets)
+
+        binding.targetRv.layoutManager = LinearLayoutManager(binding.root.context)
+        binding.targetRv.adapter = targetAdapter
+    }
 }
